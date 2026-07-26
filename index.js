a8be204626935020f55571829c79b8e65f50b4e5
Nibir Alam <nibiralpha@gmail.com>
2026-07-26 13:58:49 +0600

    minor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; 
 
 // Enable CORS middleware so your React frontend can read this data without errors
 app.use((req, res, next) => {
